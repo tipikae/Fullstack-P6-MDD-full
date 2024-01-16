f711100b9a61b5ce9ad56b5e4b1ed0dab83de894
tipikae <gilles.bd@free.fr>
2024-01-16 11:01:44 +0100

    Repositories tested

---
+++ back/src/test/java/com/openclassrooms/mddapi/integration/repository/TopicRepositoryITest.java
@@ -0,0 +1,35 @@
+package com.openclassrooms.mddapi.integration.repository;
+
+import com.openclassrooms.mddapi.model.Topic;
+import com.openclassrooms.mddapi.repository.TopicRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class TopicRepositoryITest {
+
+    @Autowired
+    TopicRepository topicRepository;
+
+    @Test
+    void test() {
+        // save
+        Topic topic = topicRepository.save(Topic.builder().name("topic-name").build());
+        assertNotNull(topic);
+
+        // get all
+        assertFalse(topicRepository.findAll().isEmpty());
+
+        // get one by id
+        assertTrue(topicRepository.existsById(topic.getId()));
+
+        // delete
+        topicRepository.deleteById(topic.getId());
+        assertFalse(topicRepository.existsById(topic.getId()));
+    }
+}

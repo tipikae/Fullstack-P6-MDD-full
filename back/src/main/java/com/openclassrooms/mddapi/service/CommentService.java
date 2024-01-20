8b7ea5d705ca5f1d1f0b2404145c4e1d4f854fd6
tipikae <gilles.bd@free.fr>
2024-01-20 09:30:50 +0100

    Comment service implemented and tested

--- back/src/main/java/com/openclassrooms/mddapi/service/CommentService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/CommentService.java
@@ -1,10 +1,48 @@
 package com.openclassrooms.mddapi.service;
 
+import com.openclassrooms.mddapi.exception.NotFoundException;
+import com.openclassrooms.mddapi.model.Comment;
+import com.openclassrooms.mddapi.model.Post;
+import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.repository.CommentRepository;
+import com.openclassrooms.mddapi.repository.PostRepository;
+import com.openclassrooms.mddapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CommentService implements ICommentService {
 
     @Autowired
     private CommentRepository commentRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Comment addComment(Comment comment, long authorId, long postId) throws NotFoundException {
+        User user = userRepository.findById(authorId).orElse(null);
+        Post post = postRepository.findById(postId).orElse(null);
+        if (user == null || post == null) {
+            String message = user == null ?
+                    String.format("User with id = %d is not found.", authorId)
+                    : String.format("Post with id = %d is not found.", postId);
+            throw new NotFoundException(message);
+        }
+
+        comment.setAuthor(user);
+        comment.setPost(post);
+
+        return commentRepository.save(comment);
+    }
+
+    @Override
+    public List<Comment> getCommentsByPostId(long postId) {
+        return commentRepository.findByPostId(postId);
+    }
 }

2b799d6f17ae5892571be0f5f61fc56175b647e5
tipikae <gilles.bd@free.fr>
2024-01-22 11:03:31 +0100

    FindByEmail method added

--- back/src/main/java/com/openclassrooms/mddapi/repository/UserRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.openclassrooms.mddapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmailOrUsername(String email, String username);
+
+    Optional<User> findByEmail(String email);
 }

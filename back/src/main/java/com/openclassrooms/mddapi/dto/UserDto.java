625504f206dd1f43cf63df6f1c875c35f03e10f0
tipikae <gilles.bd@free.fr>
2024-01-22 17:17:30 +0100

    Register request POJO created

--- back/src/main/java/com/openclassrooms/mddapi/dto/UserDto.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/UserDto.java
@@ -19,21 +19,9 @@ public class UserDto {
 
     private Long id;
 
-    @Size(max = 50, message = "Username length must be 50 max.")
-    @NotBlank(message = "Username must not be empty.")
     private String username;
 
-    @Size(max = 255, message = "Email length must be 255 max.")
-    @Email(message = "Email must have email format.")
-    @NotBlank(message = "Email must not be empty.")
     private String email;
-
-    @JsonIgnore
-    @Size(min = 8, max = 255, message = "Password length must be between 8 and 255.")
-    @Pattern(
-            regexp = "(?=.*[0-9])(?=.*[!:;,])(?=.*[a-z])(?=.*[A-Z]).*",
-            message = "Password must contain at least one number, one uppercase, one lowercase and one special character.")
-    private String password;
 
     private List<Long> topicIds;
 

c7665b7553326ed2d16ac36e37af98e9ba80c079
tipikae <gilles.bd@free.fr>
2024-01-22 11:05:13 +0100

    User added

--- ressources/sql/script.sql
+++ ressources/sql/script.sql
@@ -45,6 +45,8 @@ ALTER TABLE `POSTS` ADD FOREIGN KEY (`topic_id`) REFERENCES `TOPICS`(`id`);
 ALTER TABLE `COMMENTS` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS`(`id`);
 ALTER TABLE `COMMENTS` ADD FOREIGN KEY (`post_id`) REFERENCES `POSTS`(`id`);
 
+INSERT INTO USERS (username, email, password) VALUES ('test', 'test@test.com', '$2a$12$og6c1GH7Il1pMDJMuWgrmeN9eaPOwKR.os6upQyxdVv87gTlxLMhO');
+
 INSERT INTO TOPICS (name) VALUES ('JavaScript');
 INSERT INTO TOPICS (name) VALUES ('Java');
 INSERT INTO TOPICS (name) VALUES ('Python');

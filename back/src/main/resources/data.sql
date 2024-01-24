fd72ff92052a0cfcc20ea45d9917b29424f7a283
tipikae <gilles.bd@free.fr>
2024-01-24 15:11:23 +0100

    Data loaded on boot for H2 database

---
+++ back/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS (username, email, password) VALUES ('test', 'test@test.com', '$2a$12$og6c1GH7Il1pMDJMuWgrmeN9eaPOwKR.os6upQyxdVv87gTlxLMhO');
+INSERT INTO TOPICS (name) VALUES ('JavaScript');
+INSERT INTO TOPICS (name) VALUES ('Java');
+INSERT INTO TOPICS (name) VALUES ('Python');
+INSERT INTO TOPICS (name) VALUES ('Web3');
+INSERT INTO TOPICS (name) VALUES ('Spring');
+INSERT INTO TOPICS (name) VALUES ('Angular');

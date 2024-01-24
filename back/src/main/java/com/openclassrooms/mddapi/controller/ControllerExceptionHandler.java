dbec5d4a01a907e6613f1b06a50a5f312e763970
tipikae <gilles.bd@free.fr>
2024-01-24 17:11:22 +0100

    Bad request added

--- back/src/main/java/com/openclassrooms/mddapi/controller/ControllerExceptionHandler.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.openclassrooms.mddapi.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.mddapi.exception.AlreadyExistsException;
+import com.openclassrooms.mddapi.exception.BadRequestException;
 import com.openclassrooms.mddapi.exception.NotFoundException;
 import com.openclassrooms.mddapi.payload.response.ErrorResponse;
 import jakarta.validation.ConstraintViolationException;
@@ -19,6 +20,13 @@ import java.util.Map;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    ErrorResponse exceptionHandler(BadRequestException e) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)

e68ce955616cc334fbc21c995ba4f91b05032f96
tipikae <gilles.bd@free.fr>
2024-01-28 10:50:16 +0100

    Material added

--- front/src/app/shared/material.module.ts
+++ front/src/app/shared/material.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from "@angular/core";
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     exports: [
-        MatToolbarModule
+        MatToolbarModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule
     ]
 })
 export class MaterialModule {}

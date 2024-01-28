1d3352cdf3476e97f0ffcd047427de625fcc9dbf
tipikae <gilles.bd@free.fr>
2024-01-28 10:48:52 +0100

    Auth routing module added

--- front/src/app/features/auth/auth-routing.module.ts
+++ front/src/app/features/auth/auth-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 
 const routes: Routes = [
-
+    { title: 'Login', path: 'login', component: LoginComponent },
+    { title: 'Register', path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({

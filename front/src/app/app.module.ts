4c24ab66777af2041219170d13f3c73daa1bcf74
tipikae <gilles.bd@free.fr>
2024-01-28 10:53:39 +0100

    App module updated

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TopicComponent } from './topic/topic.component';
+import { TopicComponent } from './features/topic/topic.component';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';

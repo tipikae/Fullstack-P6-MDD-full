1290ce419d5812053496f478e3c664831a32b161
tipikae <gilles.bd@free.fr>
2024-01-30 15:41:52 +0100

    HttpClientModule commented

--- front/src/app/core/core.module.ts
+++ front/src/app/core/core.module.ts
@@ -15,7 +15,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     RouterModule,
-    HttpClientModule,
+    //HttpClientModule,
     BrowserAnimationsModule,
     SharedModule
   ],

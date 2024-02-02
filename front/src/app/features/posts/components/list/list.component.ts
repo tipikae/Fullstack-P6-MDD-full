1ff5b4ba059adb41d533bd3ef91f0345eb71b738
tipikae <gilles.bd@free.fr>
2024-02-02 03:49:49 +0100

    Post list component added

---
+++ front/src/app/features/posts/components/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { BehaviorSubject } from 'rxjs';
+import { Post } from '../../models/post.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent implements OnInit {
+
+  public posts$: BehaviorSubject<Post[]> = new BehaviorSubject<Post[]>([]);
+  public onError: boolean = false;
+
+  constructor (private postService: PostService,
+               private router: Router) {}
+  
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe({
+      next: (posts: Post[]) => this.posts$.next(posts),
+      error: _ => this.onError = true
+    });
+  }
+}

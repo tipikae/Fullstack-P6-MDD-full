dbe88dd6a1dd9015d1334b56ea1b64ac22db9800
tipikae <gilles.bd@free.fr>
2024-02-02 02:00:35 +0100

    Topic component removed

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TopicComponent } from './features/topic/topic.component';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,7 +17,6 @@ import { HomeComponent } from './components/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TopicComponent,
     MyTopicsComponent,
     ListComponent,
     MeComponent,

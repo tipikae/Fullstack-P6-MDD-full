2063aa72b74c974557e5bef79a6e6257afcdac0e
tipikae <gilles.bd@free.fr>
2024-02-03 18:45:40 +0100

    Post detail updated

--- front/src/app/features/posts/components/detail/detail.component.ts
+++ front/src/app/features/posts/components/detail/detail.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from '../../models/post.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css'
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
 
+  public onError = false;
+  public post: Post | undefined;
+  public postId!: number;
+
+  constructor (private postService: PostService,
+               private router: Router,
+               private activatedRoute: ActivatedRoute) {}
+  
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id != null) {
+      this.postId = +id;
+      this.postService.getPost(+id).subscribe({
+        next: (post: Post) => this.post = post,
+        error: _ => this.onError = true
+      });
+    } else {
+      this.onError = true;
+    }
+  }
 }

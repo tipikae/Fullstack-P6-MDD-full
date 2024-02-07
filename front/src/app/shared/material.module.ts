a9bd593576308b44ac4aeb0271d0b9db94e19614
tipikae <gilles.bd@free.fr>
2024-02-07 09:52:26 +0100

    Sidenav module added

--- front/src/app/shared/material.module.ts
+++ front/src/app/shared/material.module.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
     exports: [
@@ -17,7 +18,8 @@ import { MatSelectModule } from '@angular/material/select';
         MatIconModule,
         MatButtonModule,
         MatSnackBarModule,
-        MatSelectModule
+        MatSelectModule,
+        MatSidenavModule
     ]
 })
 export class MaterialModule {}
